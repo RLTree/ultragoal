c5d9a7b966f82fe8598be3ce94f6736f85316092
Terry Noblin <tnoblin@umich.edu>
2026-07-17 20:18:57 -0700

    Name routine custody ledger modules by behavior

--- validator/src/routine_work/runtime_adapter/production/custody/store/supported/mod.rs
+++ validator/src/routine_work/runtime_adapter/production/custody/store/supported/mod.rs
@@ -8,36 +8,29 @@ use std::os::unix::fs::{MetadataExt, OpenOptionsExt};
 use std::path::{Path, PathBuf};
 use std::time::{SystemTime, UNIX_EPOCH};
 
-#[path = "file_ledger_failure.rs"]
-mod file_ledger_failure;
-#[path = "file_ledger_open_or_initialize.rs"]
-mod file_ledger_open_or_initialize;
-#[path = "file_ledger_output.rs"]
-mod file_ledger_output;
-#[path = "file_ledger_reserve.rs"]
-mod file_ledger_reserve;
-#[path = "file_ledger_settle.rs"]
-mod file_ledger_settle;
-#[path = "file_ledger_transaction.rs"]
-mod file_ledger_transaction;
+mod failure;
 #[path = "initial_state.rs"]
 mod initial_state;
 #[path = "ledger_effect_adapter.rs"]
 mod ledger_effect_adapter;
+mod open_or_initialize;
+mod output;
 #[path = "output_journal_validation.rs"]
 mod output_journal_validation;
 #[path = "record_authentication.rs"]
 pub(super) mod record_authentication;
 #[path = "record_identity.rs"]
 mod record_identity;
+mod reserve;
+mod settle;
 #[path = "state_publication.rs"]
 mod state_publication;
 #[path = "store_open.rs"]
 mod store_open;
 #[path = "store_stat_name.rs"]
 mod store_stat_name;
+mod transaction;
 
-use file_ledger_transaction::PublicationContext;
 use initial_state::*;
 use ledger_effect_adapter::*;
 use output_journal_validation::*;
@@ -47,3 +40,4 @@ use record_identity::*;
 pub(crate) use state_publication::{
     set_test_publication_ambiguity_after, set_test_publication_refusal_after,
 };
+use transaction::PublicationContext;
