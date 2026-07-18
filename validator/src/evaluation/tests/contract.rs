1a5fd9c9f8dfb56e9ed49df31324ae312493fd45
Terry Noblin <tnoblin@umich.edu>
2026-07-18 11:13:38 -0700

    Correct evaluation test include paths

--- validator/src/evaluation/tests/contract.rs
+++ validator/src/evaluation/tests/contract.rs
@@ -12,12 +12,10 @@ use std::fs;
 use std::path::{Path, PathBuf};
 use std::sync::atomic::{AtomicU64, Ordering};
 
-include!("../../../../tests/evaluation_contract/next_root.rs");
-include!("../../../../tests/evaluation_contract/test_review_authority_authority_id.rs");
+include!("../../../tests/evaluation_contract/next_root.rs");
+include!("../../../tests/evaluation_contract/test_review_authority_authority_id.rs");
+include!("../../../tests/evaluation_contract/known_training_overlap_is_a_contamination_finding.rs");
+include!("../../../tests/evaluation_contract/self_review_cannot_promote.rs");
 include!(
-    "../../../../tests/evaluation_contract/known_training_overlap_is_a_contamination_finding.rs"
-);
-include!("../../../../tests/evaluation_contract/self_review_cannot_promote.rs");
-include!(
-    "../../../../tests/evaluation_contract/missing_observed_reward_control_blocks_run_not_just_promotion.rs"
+    "../../../tests/evaluation_contract/missing_observed_reward_control_blocks_run_not_just_promotion.rs"
 );
